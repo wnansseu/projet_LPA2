0e1749ccbae7b4b863bc0e9b1c5434b85cd49c00
william.nansseu-tchanou <william.nansseu-tchanou@utbm.fr>
2016-11-21 08:09:34 -0500

    my app

--- test/helpers/cities_helper_test.rb
+++ test/helpers/cities_helper_test.rb
@@ -1,4 +1,23 @@
 require 'test_helper'
 
 class CitiesHelperTest < ActionView::TestCase
+   setup do
+
+   @city = cities(:one)
+
+   @cityWrong = cities(:two)
+
+ end
+   test "should create city" do
+     VCR.use_cassette("nominatim") do
+
+      assert_difference('City.count') do
+
+       city = City.new  name: @city.name
+
+       city.save
+
+     end
+   end
+ end
 end
